d8baf65fd7d479ff97d0b06fb9261b4eaa121422
Beard.core <ykpro3838@gmail.com>
2026-07-14 19:06:22 +0200

    Update products.js

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const products = [
 {
     name: "Nebula HUD V2",
     price: "4.99€",
-    image: "assets/img/hud.png",
+    image: "img/hud.png",
     description: "HUD moderne, optimisé et entièrement configurable. Compatible ESX, QBCore et Standalone.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -11,7 +11,7 @@ const products = [
 {
     name: "Nebula Inventory",
     price: "9.99€",
-    image: "assets/img/inventory.png",
+    image: "img/inventory.png",
     description: "Inventaire moderne avec drag & drop, raccourcis clavier et optimisation complète.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -19,7 +19,7 @@ const products = [
 {
     name: "Nebula Garage",
     price: "7.99€",
-    image: "assets/img/garage.png",
+    image: "img/garage.png",
     description: "Garage complet avec sauvegarde des véhicules, impound et configuration simple.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -27,7 +27,7 @@ const products = [
 {
     name: "Nebula Scoreboard",
     price: "3.99€",
-    image: "assets/img/scoreboard.png",
+    image: "img/scoreboard.png",
     description: "Scoreboard moderne affichant joueurs connectés, métiers et statistiques.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -35,7 +35,7 @@ const products = [
 {
     name: "Nebula Admin Menu",
     price: "14.99€",
-    image: "assets/img/admin.png",
+    image: "img/admin.png",
     description: "Menu administrateur complet avec gestion des joueurs, véhicules et sanctions.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -43,7 +43,7 @@ const products = [
 {
     name: "Nebula Character Creator",
     price: "11.99€",
-    image: "assets/img/creator.png",
+    image: "img/creator.png",
     description: "Créateur de personnage moderne avec aperçu en temps réel et sauvegarde.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -51,7 +51,7 @@ const products = [
 {
     name: "Nebula Banking",
     price: "8.99€",
-    image: "assets/img/bank.png",
+    image: "img/bank.png",
     description: "Système bancaire avec virements, historique des transactions et cartes bancaires.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -59,7 +59,7 @@ const products = [
 {
     name: "Nebula Phone",
     price: "19.99€",
-    image: "assets/img/phone.png",
+    image: "img/phone.png",
     description: "Téléphone RP complet avec appels, SMS, contacts, applications et réseaux sociaux.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -67,7 +67,7 @@ const products = [
 {
     name: "Nebula Housing",
     price: "15.99€",
-    image: "assets/img/housing.png",
+    image: "img/housing.png",
     description: "Système de maisons avec achat, stockage, clés et gestion des propriétaires.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 },
@@ -75,7 +75,7 @@ const products = [
 {
     name: "Nebula Clothing",
     price: "6.99€",
-    image: "assets/img/clothing.png",
+    image: "img/clothing.png",
     description: "Magasin de vêtements moderne avec sauvegarde des tenues et favoris.",
     link: "https://www.patreon.com/cw/NebulaPaye"
 }
